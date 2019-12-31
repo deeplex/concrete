6b84aaa9d149b437558f547c59fcd8b8f50b6c1c
Henrik S. Gassmann <henrik@gassmann.onl>
2019-12-31 14:05:34 +0100

    Add overloaded lambda helper type

---
+++ include/dplx/overloaded.hpp
@@ -0,0 +1,19 @@
+
+// Copyright Henrik Steffen Gaßmann 2019
+//
+// Distributed under the Boost Software License, Version 1.0.
+//         (See accompanying file LICENSE or copy at
+//           https://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+namespace dplx
+{
+    template <typename... Ts>
+    struct overloaded : Ts...
+    {
+        using Ts::operator()...;
+    };
+    template <typename... Ts>
+    overloaded(Ts...)->overloaded<Ts...>;
+} // namespace dplx
