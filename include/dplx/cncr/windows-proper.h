1140f594cbcd651548e9f415db369285b3f3657c
Henrik S. Gaßmann <henrik@gassmann.onl>
2020-01-03 10:49:53 +0100

    Add lean <windows.h> wrapper header
    Add a header which trims down the scope of `<windows.h>` definitions by
    defining some exclusion macros. `<windows.h>` should only be included
    indirectly via the wrapper -- if it is included directly before the
    wrapper, the exclusion macros won't have any effect.

---
+++ include/dplx/cncr/windows-proper.h
@@ -0,0 +1,21 @@
+
+// Copyright Henrik Steffen Gaßmann 2018
+//
+// Distributed under the Boost Software License, Version 1.0.
+//         (See accompanying file LICENSE or copy at
+//           https://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#if __has_include(<SDKDDKVer.h>) && (_WIN32 || _WIN32_WINNT)
+// _WIN32_WINNT is defined per compiler flag
+#include <SDKDDKVer.h>
+
+#define VC_EXTRALEAN 1
+#define WIN32_LEAN_AND_MEAN 1
+
+#define NOMINMAX 1
+
+#include <windows.h>
+
+#endif
