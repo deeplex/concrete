48db470f5348d1df67e94bfcfefad04c4bd17827
Henrik S. Gaßmann <henrik.gassmann@adesso.de>
2022-07-28 13:40:19 +0200

    Open version v0.0.0-alpha.5

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Henrik Steffen Gaßmann'
 author = 'Henrik Steffen Gaßmann'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.0-alpha.4'
+release = '0.0.0-alpha.5'
 
 
 # -- General configuration ---------------------------------------------------
